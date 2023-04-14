e9e0b6a0f4bb797ec5318946b38672dc0c265866
Guada <guadapdls@gmail.com>
2023-04-14 16:15:41 -0300

    Inicializo firebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="App">
-      
+      <button onClick={() => console.log(import.meta.env.VITE_PROJECT_ID)}></button>
     </div>
   )
 }
---
+++ src/Services/Auth/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {getAuth} from 'firebase/auth'
+
+const Auth = () => {
+
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth

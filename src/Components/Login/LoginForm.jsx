c129787aee4ad9db89ad82a254127f5c36e15943
Lucas Becerra <lucaseramos13@gmail.com>
2023-04-17 19:08:36 -0300

    creacion de hook useForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,12 +1,49 @@
 import { Button, Input } from '@mui/joy';
 import React from 'react'
+import { useForm } from '../../Hooks/useForm';
 import './LoginForm.css'
 
-const LoginForm = () => {
+const initialForm = {
+  email: "",
+  password:""
+};
 
-  function handleSubmit(e) {
-    e.preventDefault();
+const validationsForm = (form) => {
+  let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+
+
+  if (!form.password.trim()) {
+    errors.password = "El campo 'Nombre' es requerido";
+  } else if (!regexName.test(form.name)) {
+    errors.password = "El campo 'Nombre' sólo acepta letras y espacios en blanco";
   }
+
+  if (!form.email.trim()) {
+    errors.email = "El campo 'Email' es requerido";
+  } else if (!regexEmail.test(form.email.trim())) {
+    errors.email = "El campo 'Email' es incorrecto";
+  }
+
+  return errors;
+};
+
+
+const LoginForm = () => {
+  const {
+    form,
+    errors,
+    loading,
+    response,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+
+  } = useForm(initialForm, validationsForm);
+
+
+
 
   return (
     <section className='container-login'>
@@ -15,25 +52,34 @@ const LoginForm = () => {
       </div>
 
       <div className='container-form-login'>
-        <form className='login-form' onSubmit={(e) => handleSubmit(e)}>
+        <form className='login-form' onSubmit={handleSubmit}>
           <label htmlFor="">Email</label>
           <Input
-            color="neutral"
-            disabled={false}
-            placeholder="Email"
+            placeholder="Type your email"
             variant="outlined"
+            type="email"
+            name="email"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.email}
+            required
           />
+          {errors.email && <p>{errors.email}</p>}
           <label htmlFor="">Password</label>
           <Input
-            color="neutral"
-            disabled={false}
             placeholder="Password"
             variant="outlined"
+            type="text"
+            name="password"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.password}
+            required
           />
-
+          {errors.password && <p>{errors.password}</p>}
           <Button
+          type="submit"
             color="neutral"
-            onClick={()=> console.log('ola')}
             size="md"
             variant="solid"
           >Ingresar</Button>
